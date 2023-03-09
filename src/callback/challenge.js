981ede0f27b9de0c9dbdba0546ead8005e933b59
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2023-03-09 02:02:47 -0600

    Callback hell

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -5,7 +5,7 @@ function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
 
     xhttp.open('GET', urlApi, true);
-    xhttp.onreadystatechange = function (event) {
+    xhttp.onreadystatechange = function (event){
 
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
@@ -20,9 +20,9 @@ function fetchData(urlApi, callback) {
 }
 fetchData(`${API}/products`, function (error1, data1) {
     if (error1) return console.error(error1);
-    fetchData(`${API}/products/${data[0].id}`, function (error2, data2) {
-        if (error2) return console.error.apply(error2);
-        fetchData(`${API}/categories/${data2?.category.id}`, function (error3, data3) {
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3){
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
